19b4217f29b71088a01dcbb42056093b2a1f0c61
Polevoy Yevhen <polevoye90@gmail.ua>
2025-09-13 16:59:10 +0300

    create app user, create models, setting params as a AUTH_USER_MODEL

--- box_office/admin.py
+++ box_office/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from box_office.models import Play
 
-# Register your models here.
+
+admin.site.register(Play)
--- theater_service/settings.py
+++ theater_service/settings.py
@@ -37,8 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework"
     "box_office",
+    "user"
 ]
+
+AUTH_USER_MODEL = "user.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
